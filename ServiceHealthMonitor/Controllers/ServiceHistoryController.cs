fe0c0baae03526611ec3a42d33457e9738d83490
vamsipraveen-dt <vamsi.praveen@iamdigitaltrust.com>
2026-03-30 12:10:58 +0530

    Update ServiceHistoryController.cs

--- ServiceHealthMonitor/Controllers/ServiceHistoryController.cs
+++ ServiceHealthMonitor/Controllers/ServiceHistoryController.cs
@@ -7,6 +7,7 @@ namespace ServiceHealthMonitor.Controllers
 {
     [Route("api")]
     [ApiController]
+    [AllowAnonymous]
     public class ServiceHistoryController : Controller
     {
         private readonly ILogger<ServiceHistoryController> _logger;
